e015dd54020e0583f922fbe44411a83a04cbda1a
Mehmet Ali DALĞIÇ <malidalgic51@gmail.com>
2024-06-27 23:55:19 +0300

    initial

---
+++ src/main/java/com/carla/hiringcase/HiringCaseApplication.java
@@ -0,0 +1,13 @@
+package com.carla.hiringcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiringCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carla/hiringcase/HiringCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carla.hiringcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
